e8fb58ed9c3c8be8a363fa8a345f1d60f034d822
Michal Sierpinski <msierpinski@outlook.com>
2020-10-03 18:04:20 +0200

    navigate to welcome screen if user not logged in

--- lib/ui/views/startup/startup_view_model.dart
+++ lib/ui/views/startup/startup_view_model.dart
@@ -5,36 +5,14 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class StartupViewModel extends BaseViewModel {
-  bool isSignedIn = false;
   final NavigationService _navigationservice = locator<NavigationService>();
 
   Future initialize() async {
-    await Future.delayed(Duration(seconds: 2));
-
     var userInfo = await FirebaseAuth.instance.currentUser();
     if (userInfo != null) {
       await _navigationservice.replaceWith(Routes.dashboardView);
     } else {
-      await _navigationservice.replaceWith(Routes.loginView);
-    }
-  }
-
-  Future navigateToLoginScreen() async {
-    await _navigationservice.navigateTo(Routes.loginView);
-  }
-
-  void goForward() {
-    _navigationservice.replaceWith(Routes.dashboardView);
-  }
-
-  void signInAnonymously() async {
-    try {
-      final authResult = await FirebaseAuth.instance.signInAnonymously();
-      isSignedIn = true;
-      notifyListeners();
-      print('${authResult.user.uid}');
-    } catch (e) {
-      print(e.toString());
+      await _navigationservice.replaceWith(Routes.welcomeView);
     }
   }
 }
