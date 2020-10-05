9347875ceb8d1746a5766570a3b63b57ad4d9dc9
MSierpinski <msierpinski@outlook.com>
2020-10-05 17:41:28 +0200

    permission service

--- lib/services/permission_servic.dart
+++ lib/services/permission_servic.dart
@@ -1,7 +1,44 @@
+import 'package:dogtour_admin/app/locator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:injectable/injectable.dart';
 
+import 'user_service.dart';
+
+enum UserPermission {
+  createModerator,
+  addPet,
+  deletePet,
+  editPet,
+  walkPet,
+  adoptPet,
+  sponsorPet,
+}
+
 @injectable
 class PermissionService {
-  
+  final UserService _userService = locator<UserService>();
+
+  var userPermissions = [
+    UserPermission.walkPet,
+    UserPermission.adoptPet,
+    UserPermission.sponsorPet
+  ];
+  var moderatorPermissions = [
+    UserPermission.addPet,
+    UserPermission.deletePet,
+    UserPermission.editPet
+  ];
+
+  bool canUser(UserPermission permission) {
+    var user = _userService.getUser();
+
+    if (user.userRole == "admin") {
+      return true;
+    } else if (user.userRole == "user") {
+      return userPermissions.contains(permission);
+    } else if (user.userRole == "moderator") {
+      return moderatorPermissions.contains(permission);
+    }
+    return false;
+  }
 }
