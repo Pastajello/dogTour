33fbe6b6af5af505387fd5f79e7a290569e04c87
MSierpinski <msierpinski@outlook.com>
2020-10-05 17:41:45 +0200

    get user profile at login

--- lib/ui/views/login/login_view_model.dart
+++ lib/ui/views/login/login_view_model.dart
@@ -1,22 +1,27 @@
 import 'package:dogtour_admin/app/locator.dart';
 import 'package:dogtour_admin/app/router.gr.dart';
 import 'package:dogtour_admin/services/authetication_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import 'package:dogtour_admin/services/firestore_service.dart';
+import 'package:dogtour_admin/services/user_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class LoginViewModel extends BaseViewModel {
   final NavigationService _navigationservice = locator<NavigationService>();
+  final FirestoreService _firestoreService = locator<FirestoreService>();
   final AutheticationService _autheticationService =
       locator<AutheticationService>();
+  final UserService _userService = locator<UserService>();
 
   String email;
   String password;
 
   Future signIn() async {
     try {
-      await _autheticationService.signInWithEmail(email, password);
+      var authResult =
+          await _autheticationService.signInWithEmail(email, password);
+      var user = await _firestoreService.getUserProfile(authResult.user.uid);
+      _userService.setUser(user);
       _navigationservice.replaceWith(Routes.dashboardView);
     } catch (e) {
       print(e.toString());
