f577a347ba0944e1a5630373706340d15065e228
Michal Sierpinski <msierpinski@outlook.com>
2020-10-14 21:13:54 +0200

    reserve a hour walk with a pet

--- lib/ui/views/pet/pet_calendar/pet_calendar_view.dart
+++ lib/ui/views/pet/pet_calendar/pet_calendar_view.dart
@@ -13,18 +13,59 @@ class PetCalendarView extends StatelessWidget {
     var routeData = RouteData.of(context).arguments as List;
     return ViewModelBuilder<PetCalendarViewModel>.reactive(
         builder: (context, model, child) => Scaffold(
-                body: Container(
-              color: Colors.white,
-              child: Column(
-                children: [
-                  TableCalendar(
-                    calendarController: _calendarController,
-                    calendarStyle: CalendarStyle(
-                        weekdayStyle: TextStyle(color: Colors.black)),
-                  )
-                ],
-              ),
-            )),
+              appBar: AppBar(),
+              body: Container(
+                  color: Colors.white,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        FlatButton(
+                            onPressed: model.reserveSpot,
+                            child: Text("Go for a walk!")),
+                        TableCalendar(
+                          enabledDayPredicate: (day) {
+                            var now = DateTime.now();
+                            var isToday = day.year == now.year &&
+                                day.month == now.month &&
+                                day.day == now.day;
+                            return day.isAfter(DateTime.now()) || isToday;
+                          },
+                          calendarController: _calendarController,
+                          onDaySelected: (day, events) {
+                            model.changeSelectedDay(day);
+                          },
+                          initialCalendarFormat: CalendarFormat.twoWeeks,
+                          initialSelectedDay: DateTime.now(),
+                          calendarStyle: CalendarStyle(
+                              weekdayStyle: TextStyle(color: Colors.black)),
+                        ),
+                        ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: model.spots.length,
+                          itemBuilder: (context, index) {
+                            var spot = model.spots[index];
+                            return InkWell(
+                                splashColor: Colors.red,
+                                focusColor: Colors.green,
+                                highlightColor: Colors.blue,
+                                hoverColor: Colors.purple,
+                                onTap: () {
+                                  model.changeSelectedHour(spot);
+                                },
+                                child: Card(
+                                    child: ListTile(
+                                  selectedTileColor: Colors.red,
+                                  selected: model.selectedHour == spot,
+                                  focusColor: Colors.red,
+                                  title: Text(spot.toString()),
+                                )));
+                          },
+                        )
+                      ],
+                    ),
+                  )),
+            ),
         onModelReady: (model) async {
           await model.init(context);
         },
--- lib/ui/views/pet/pet_calendar/pet_calendar_view_model.dart
+++ lib/ui/views/pet/pet_calendar/pet_calendar_view_model.dart
@@ -3,30 +3,66 @@ import 'package:dogtour_admin/models/pet.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:dogtour_admin/models/reserved_spot.dart';
+import 'package:dogtour_admin/services/firestore_service.dart';
+import 'package:dogtour_admin/services/user_service.dart';
 
 class PetCalendarViewModel extends BaseViewModel {
   final NavigationService _navigationservice = locator<NavigationService>();
+  final FirestoreService _firestoreService = locator<FirestoreService>();
+  final UserService _userService = locator<UserService>();
 
   Pet pet;
 
   bool isFavourite;
-
+  List<ReservedSpot> takenSpots = [];
+  List<int> spots = [];
+  DateTime selectedDay;
+  int selectedHour;
   PetCalendarViewModel(Pet petDetails) {
     pet = petDetails;
     isFavourite = false;
   }
-  Future<void> init(BuildContext context) async {}
-
-  void goBack() {
-    _navigationservice.back();
-  }
-
-  void setAsFavourite() {
-    isFavourite = !isFavourite;
+  Future<void> init(BuildContext context) async {
+    try {
+      takenSpots = await _firestoreService.getReservedSpots(pet.id);
+      changeSelectedDay(DateTime.now());
+    } catch (e) {
+      print(e);
+    }
     notifyListeners();
   }
 
-  void showPicture(Widget image) {
-    _navigationservice.navigateToView(image);
+  changeSelectedDay(DateTime day) {
+    spots = List.generate(24, (index) => index);
+    selectedDay = day;
+    var takenSportsForTheDay = takenSpots.where((element) =>
+        element.start.day == selectedDay.day &&
+        element.start.month == selectedDay.month &&
+        element.start.year == selectedDay.year);
+    spots.removeWhere((element) =>
+        takenSportsForTheDay.any((taken) => element == taken.start.hour));
+    notifyListeners();
+  }
+
+  changeSelectedHour(int hour) {
+    selectedHour = hour;
+    notifyListeners();
+  }
+
+  reserveSpot() async {
+    var start = DateTime(
+        selectedDay.year, selectedDay.month, selectedDay.day, selectedHour);
+    await _firestoreService.addSpotToPetCalendar(
+        pet,
+        ReservedSpot(
+            start: start,
+            end: start.add(Duration(hours: 1)),
+            userId: _userService.getUser().id));
+    _navigationservice.back();
+  }
+
+  void goBack() {
+    _navigationservice.back();
   }
 }
